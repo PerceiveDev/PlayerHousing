942223a2de4ea0414add96cf9c8fef3321791fd1
Rayzr522 <rayzr522@gmail.com>
2016-10-28 10:29:36 -0400

    Added some variables that will be need in the future

--- src/main/java/com/perceivedev/playerhousing/housing/House.java
+++ src/main/java/com/perceivedev/playerhousing/housing/House.java
@@ -1,9 +1,11 @@
 package com.perceivedev.playerhousing.housing;
 
 
-import org.bukkit.Location;
-
 import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Location;
 
 /**
  * Created by jan on 9/30/16.
@@ -12,6 +14,9 @@ public class House {
     /*House Schematic?*/
     private Location houseSpawnPoint;
     private Location houseLocation;
+    private boolean locked;
+    private List<UUID> banned;
+    
 
     public House(Location houseLocation, Location houseSpawnPoint, File schematic) {
         this.houseSpawnPoint = houseSpawnPoint;
